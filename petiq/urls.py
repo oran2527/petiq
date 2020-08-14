53a37ff2825ae2ad31478d2baa1905ca3ed846f0
oran2527 <orlago250183@gmail.com>
2020-08-14 19:14:55 +0000

    upload changes not done

--- petiq/urls.py
+++ petiq/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf.urls import url
-from petiqrContainer import views
+from petiqContainer import views
 from django.conf import settings
 from django.conf.urls.static import static
 
