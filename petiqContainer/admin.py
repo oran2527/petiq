c0365befd33f21c3472bfe60ced56bb568a1cbce
oran2527 <orlago250183@gmail.com>
2020-08-14 17:24:18 +0000

    registering petiq models

--- petiqContainer/admin.py
+++ petiqContainer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Countries
+from .models import States
+from .models import Cities
+from .models import Owners
 
 # Register your models here.
+admin.site.register(Countries)
+admin.site.register(States)
+admin.site.register(Cities)
+admin.site.register(Owners)
